bf94e9db2ff22f1ef66adb57e5e7ec83ea316d5b
zwanz94 <141385082+zwanz94@users.noreply.github.com>
2023-08-08 23:48:01 +0900

    Add files via upload

--- 완종/pcap_0808.c
+++ 완종/pcap_0808.c
@@ -61,20 +61,14 @@ struct sniff_tcp {
 	u_short th_urp;
 };
 
-// mysql information
-#define host "localhost"
-#define user "root"
-#define password "1234"
-#define database "PROJECT"
-
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char* packet);
-void print_ethernet(const struct sniff_ethernet **ethernet);
-void print_IP(const struct sniff_ip **ip, char *IPbuffer_str, char *IPbuffer2_str);
-void print_PORT(const struct sniff_tcp **tcp, unsigned short *tcp_src_port, unsigned short *tcp_dst_port);
+void print_ethernet(const struct sniff_ethernet *ethernet);
+void print_IP(const struct sniff_ip *ip, char *IPbuffer_str, char *IPbuffer2_str);
+void print_PORT(const struct sniff_tcp *tcp, unsigned short *tcp_src_port, unsigned short *tcp_dst_port);
 void print_domain(u_char* domain_str);
 void sql_connect(MYSQL **sql_handle);
-void transfer_to_db(MYSQL **sql_handle, char *IPbuffer_str, char *IPbuffer2_str, u_char* domain_str);
-void transfer_from_db(MYSQL **sql_handle, MYSQL_RES **res, MYSQL_ROW *row, u_char *domain_str);
+void transfer_to_db(MYSQL *sql_handle, char *IPbuffer_str, char *IPbuffer2_str, u_char* domain_str);
+int transfer_from_db(MYSQL *sql_handle, MYSQL_RES *res, MYSQL_ROW *row, u_char *domain_str);
 
 int main( int argc, char *argv[])
 {
@@ -142,11 +136,16 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 	char IPbuffer2_str[16];
 	unsigned short tcp_src_port = 0;
 	unsigned short tcp_dst_port = 0;
+	int allowed_domain;
 
 	/*-----------------mysql_info-----------------*/
 	MYSQL *sql_handle; // mysql.h 구조체
 	MYSQL_RES *res;	
 	MYSQL_ROW row;
+	#define host "localhost"
+	#define user "root"
+	#define password "1234"
+	#define database "PROJECT"
 		
 	/*------------------ethernet------------------*/
 	#define SIZE_ETHERNET 14 /* ethernet headers are always exactly 14 bytes */
@@ -195,54 +194,64 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 
 	/*-----------------print data-----------------*/
 	if(domain_len){
-		print_ethernet(&ethernet);
-		print_IP(&ip, IPbuffer_str, IPbuffer2_str);
-		print_PORT(&tcp, &tcp_src_port, &tcp_dst_port);
+		print_ethernet(ethernet);
+		print_IP(ip, IPbuffer_str, IPbuffer2_str);
+		print_PORT(tcp, &tcp_src_port, &tcp_dst_port);
 		print_domain(domain_str);
 		sql_connect(&sql_handle);
-		transfer_to_db(&sql_handle,IPbuffer_str,IPbuffer2_str,domain_str);
-		transfer_from_db(&sql_handle, &res, &row, domain_str);
-		fputc('\n',stdout);
-		mysql_free_result(res);
-		mysql_close(sql_handle);
+		transfer_to_db(sql_handle,IPbuffer_str,IPbuffer2_str,domain_str);
+		allowed_domain = transfer_from_db(sql_handle, res, &row, domain_str);
+			    	
+		if(allowed_domain){ //check domain blocked/allowed
+			printf("DEBUG: domain allowed.\n");
+		}
+		else {
+			printf("DEBUG: domain blocked.\n");
+			//sendraw();
+		}
+		
+		fputc('\n',stdout);	
 	}
+	
+	
+	
 }
 // end of got_pakcet function.
 
-void print_ethernet(const struct sniff_ethernet **ethernet){
+void print_ethernet(const struct sniff_ethernet *ethernet){
 	printf("DATA: dest MAC : %02x:%02x:%02x:%02x:%02x:%02x\n",
-		(*ethernet)->ether_dhost[0],
-		(*ethernet)->ether_dhost[1],
-		(*ethernet)->ether_dhost[2],
-		(*ethernet)->ether_dhost[3],
-		(*ethernet)->ether_dhost[4],
-		(*ethernet)->ether_dhost[5]
+		ethernet->ether_dhost[0],
+		ethernet->ether_dhost[1],
+		ethernet->ether_dhost[2],
+		ethernet->ether_dhost[3],
+		ethernet->ether_dhost[4],
+		ethernet->ether_dhost[5]
 	);
 	printf("DATA: src MAC : %02x:%02x:%02x:%02x:%02x:%02x\n",
-		(*ethernet)->ether_shost[0],
-		(*ethernet)->ether_shost[1],
-		(*ethernet)->ether_shost[2],
-		(*ethernet)->ether_shost[3],
-		(*ethernet)->ether_shost[4],
-		(*ethernet)->ether_shost[5]
+		ethernet->ether_shost[0],
+		ethernet->ether_shost[1],
+		ethernet->ether_shost[2],
+		ethernet->ether_shost[3],
+		ethernet->ether_shost[4],
+		ethernet->ether_shost[5]
 	);
 }
 
-void print_IP(const struct sniff_ip **ip, char *IPbuffer_str, char *IPbuffer2_str){
+void print_IP(const struct sniff_ip *ip, char *IPbuffer_str, char *IPbuffer2_str){
 	char *IPbuffer, *IPbuffer2;
 
-	IPbuffer = inet_ntoa((*ip)->ip_src);
+	IPbuffer = inet_ntoa(ip->ip_src);
 	strcpy(IPbuffer_str, IPbuffer);
-	IPbuffer2 = inet_ntoa((*ip)->ip_dst);
+	IPbuffer2 = inet_ntoa(ip->ip_dst);
 	strcpy(IPbuffer2_str, IPbuffer2);
 	
 	printf("DATA: IP src : %s\n",IPbuffer_str);
 	printf("DATA: IP dst : %s\n",IPbuffer2_str);
 }
 
-void print_PORT(const struct sniff_tcp **tcp, unsigned short *tcp_src_port, unsigned short *tcp_dst_port){
-	*tcp_src_port = ntohs((*tcp)->th_sport);
-	*tcp_dst_port = ntohs((*tcp)->th_dport);
+void print_PORT(const struct sniff_tcp *tcp, unsigned short *tcp_src_port, unsigned short *tcp_dst_port){
+	*tcp_src_port = ntohs(tcp->th_sport);
+	*tcp_dst_port = ntohs(tcp->th_dport);
 	
 	printf("DATA: src Port : %u\n", *tcp_src_port);
 	printf("DATA: dst Port : %u\n", *tcp_dst_port);	
@@ -254,7 +263,7 @@ void print_domain(u_char* domain_str){
 
 void sql_connect(MYSQL **sql_handle){
 	*sql_handle = mysql_init(NULL);
-	if(!(*sql_handle)) {
+	if(!*sql_handle) {
 		printf("init error!\n");
 		exit(0);
 	}
@@ -262,24 +271,24 @@ void sql_connect(MYSQL **sql_handle){
 	if(!(*sql_handle)) printf("connect error!\n");
 }
 
-void transfer_to_db(MYSQL **sql_handle, char *IPbuffer_str, char *IPbuffer2_str, u_char* domain_str){
+void transfer_to_db(MYSQL *sql_handle, char *IPbuffer_str, char *IPbuffer2_str, u_char* domain_str){
 	char spr_buf[1024];
 	sprintf(spr_buf,"insert into connect_IP values (\"%s\",\"%s\",\"%s\")",IPbuffer_str,IPbuffer2_str,domain_str);
-	mysql_query(*sql_handle,spr_buf);
+	mysql_query(sql_handle,spr_buf);
 }
 
-void transfer_from_db(MYSQL **sql_handle, MYSQL_RES **res, MYSQL_ROW *row, u_char *domain_str){
+int transfer_from_db(MYSQL *sql_handle, MYSQL_RES *res, MYSQL_ROW *row, u_char *domain_str){
 	int cmp_ret; // strcmp 리턴값 저장 할 변수
 	int str_len1; // block_IP 길이
 	int str_len2; // 접속한 도메인 길이
 	str_len2 = strlen(domain_str);
 	
-	mysql_query(*sql_handle,"select domain from block_IP");
-	*res = mysql_store_result(*sql_handle); 
-	if(*res == NULL) printf("mysql_store_result error!!\n");
+	mysql_query(sql_handle,"select domain from block_IP");
+	res = mysql_store_result(sql_handle); 
+	if(res == NULL) printf("mysql_store_result error!!\n");
 	else{
 		cmp_ret = 1;
-		while(*row = mysql_fetch_row(*res)){
+		while(*row = mysql_fetch_row(res)){
 			str_len1 = strlen(*row[0]);
 			if(str_len1 != str_len2) continue;
 			cmp_ret = strcmp(*row[0],domain_str);
@@ -287,12 +296,8 @@ void transfer_from_db(MYSQL **sql_handle, MYSQL_RES **res, MYSQL_ROW *row, u_cha
 		}
 	}
 	
-	// check domain blocked/allowed
-	if(cmp_ret == 0){
-		printf("DEBUG: domain blocked.\n");
-		//sendraw();
-	}
-	else {
-		printf("DEBUG: domain allowed.\n");
-	} // end if cmp_ret
+	mysql_free_result(res);
+	mysql_close(sql_handle);
+	
+	return cmp_ret;
 }
