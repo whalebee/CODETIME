413e9b91fe1f071672ff2de7ad95e800b64b259e
whalebee <islhoeve@gmail.com>
2023-08-20 16:39:08 +0900

    test

--- Node/test.js
+++ Node/test.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const dbconfig   = require('./config/db_config.js');
 const conn = mysql.createConnection(dbconfig);
+const http = require('http');
 
-  app.get('/',function(req, res){
+app.get('/',function(req, res){
+
+    
+
 	conn.connect(); // mysql과 연결
 
     var sql = 'select * from tb_packet_block'
@@ -14,10 +18,14 @@ const conn = mysql.createConnection(dbconfig);
             console.error('error connecting: ' + err.stack);
         }
         res.send(rows);
-            
     });
     conn.end(); // 연결 해제
 }); 
+
+app.get('/' , function(req, res) {
+
+    res.sendfile('./index.html');
+})
  
 app.listen(3333, function(){
 	console.log('Listening at 3333');
