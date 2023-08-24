68879396ddf59767374cd5abe88db980c7f50a24
whalebee <islhoeve@gmail.com>
2023-08-24 17:31:15 +0900

    gogogogoo

--- project_node/main.js
+++ project_node/main.js
@@ -1,19 +1,48 @@
 var express = require('express');
-var app = express()                 
-var mysql = require('mysql')                       
+var app = express();                 
+var mysql = require('mysql');                       
 var bodyParser = require('body-parser');
-var morgan = require('morgan')    
+var morgan = require('morgan');
+// new
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
-app.use(morgan('short'))                            // log middle ware
-app.use(bodyParser.urlencoded({extended:false}))
+var authRouter = require('./lib_login/auth');
+var authCheck = require('./lib_login/authCheck.js');
+var template = require('./lib_login/template.js');
+//------------------------
+
+
+// new
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: '~~~',	// 원하는 문자 입력
+  resave: false,
+  saveUninitialized: true,
+  store:new FileStore(),
+}))
+
+
+// 인증 라우터
+app.use('/auth', authRouter);
+
+//------------------------
+
+
+app.use(morgan('short'));                            // log middle ware
+app.use(bodyParser.urlencoded({extended:false}));
+
 
 // app.use(express.static('./public'))                 // use after login function
 // var userRouter = require('./routes/user.js')
 
-var domainRouter = require('./routes/domain.js')
+var domainRouter = require('./routes/domain.js');
 // app.use(userRouter) // use after login function
-app.use(domainRouter)
+app.use(domainRouter);
+
+
+
        
 app.listen(3003,function(){
     console.log("Listening on port 3003") 
-})   
+});   
